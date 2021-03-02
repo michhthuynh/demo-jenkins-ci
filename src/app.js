24bf4d91b84a056681966d97b0d21f6fff57624d
Huynh Hoang Thanh <hthuynh.menlo@tma.com.vn>
2021-03-02 14:49:06 +0700

    init project

--- src/app.js
+++ src/app.js
@@ -2,4 +2,12 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5000
 
+app.get('/', (req, res) => {
+  res.send('Server is running')
+})
+
+app.post('/', (req, res) => {
+  res.send('Server is running')
+})
+
 app.listen(PORT, () => console.log(`Server is running at ${PORT}`))
